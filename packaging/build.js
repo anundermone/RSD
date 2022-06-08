4e3fc08723208a292ce8084ad2668f745c5a043e
anundermone <matthew.wilson@live.co.uk>
2022-06-08 19:46:05 +0100

    Added layout and page rendering

--- packaging/build.js
+++ packaging/build.js
@@ -1,15 +1,48 @@
 import nunjucks from 'nunjucks'
 import YAML from 'yaml'
-import { readFileSync, writeFileSync } from 'fs'
+import fs from 'fs'
+import path from 'path'
 
 function renderHtml() {
-    const yamlContent = readFileSync('content/content.yaml', { encoding: 'utf8' })
-    const content = YAML.parse(yamlContent, { strict: true })
-
+    const globalContent = loadYamlFile('content/global.yaml')
     const njk = nunjucks.configure({ throwOnUndefined: true })
-    const html = njk.render('pages/index.njk', content)
-    
-    writeFileSync('build/index.html', html)
+
+    for (const page of getPageNames()) {
+        const pageContent = loadYaml(page)
+        const content = { page: pageContent, ...globalContent }
+        const html = renderTemplate(njk, page, content)
+        fs.writeFileSync(`build/${page}.html`, html)
+    }
+}
+
+function getPageNames() {
+    const templates = fs.readdirSync('pages').filter(isNjk)
+    return templates.map(getPageName)
+}
+
+function getPageName(template) {
+    return template.replace('.njk', '')
+}
+
+function isNjk(file) {
+    const path = `pages/${file}`
+    return fs.lstatSync(path).isFile() && file.endsWith(".njk")
+}
+
+function loadYaml(page) {
+    const file = `content/${page}.yaml`
+    return loadYamlFile(file)
+}
+
+function loadYamlFile(file) {
+    const yamlContent = fs.readFileSync(file, { encoding: 'utf8' })
+    return YAML.parse(yamlContent, { strict: true })
+}
+
+function renderTemplate(njk, page, content) {
+    const file = `pages/${page}.njk`
+    return njk.render(file, content)
 }
 
 renderHtml()
+
