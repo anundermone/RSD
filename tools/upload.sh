8f15735cd8928f443222f105abd4e9111a3ae1c0
anundermone <matthew.wilson@live.co.uk>
2022-10-15 12:12:39 +0100

    Update CI for live deployment

---
+++ tools/upload.sh
@@ -0,0 +1,29 @@
+#/bin/sh
+set -eu
+
+echo "$SSH_KEY" > key
+
+echo 'Copying build files to server...'
+
+scp \
+    -P $SSH_PORT \
+    -i key \
+    -o StrictHostKeyChecking=no \
+    -o UserKnownHostsFile=/dev/null \
+    -v \
+    -r \
+    build/. \
+    $SSH_ADDRESS:build
+
+echo 'Replacing website files...'
+
+ssh \
+    -p $SSH_PORT \
+    -i key \
+    -o StrictHostKeyChecking=no \
+    -o UserKnownHostsFile=/dev/null \
+    -v \
+    $SSH_ADDRESS \
+    'rm -r public_html/* & cp -r build/. public_html & rm -rf build'
+
+echo 'Upload complete!'
